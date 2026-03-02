b690292cad124c9b4249f8f8dc638a976184ea36
tajMahel <maelmemain1@gmail.com>
2026-03-02 15:41:26 +0100

    fix fautes

--- test/test_Points_Vie.py
+++ test/test_Points_Vie.py
@@ -2,7 +2,7 @@ from personnage import Personnage
 
 def test_points_vie():
     # Étant donné un personnage
-    # Quand il est créé
+    # Lorsqu'il est créé
     perso = Personnage()
     # Il possède 10 points de vie
     assert perso.get_points_vie() == 10
--- test/test_subir_degats.py
+++ test/test_subir_degats.py
@@ -5,5 +5,5 @@ def test_points_vie():
     perso = Personnage()
     # Lorqu'il subit 1 dégat
     perso.subir_degats()
-    # Ses points de vie baisse de 1 point par dégat
+    # Ses points de vie baissent de 1 point par dégat
     assert perso.get_points_vie() == 9
