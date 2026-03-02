a16dccfbba6bd2e5295356198a92a0d5196c93b6
tajMahel <maelmemain1@gmail.com>
2026-03-02 15:23:09 +0100

    Ajout Partie 3

---
+++ test/test_Points_Vie.py
@@ -0,0 +1,8 @@
+from src.personnage import Personnage
+
+def test_points_vie():
+    # Étant donné un personnage
+    # Quand il est créé
+    perso = Personnage()
+    # Il possède 10 points de vie
+    assert perso.get_points_vie() == 10
