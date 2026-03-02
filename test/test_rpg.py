1fb1ecd0a8f255117df0bd723d48de5a20368023
Theo Hadj <theo.hadja@gmail.com>
2026-03-02 15:05:32 +0100

    add tests

--- test/test_rpg.py
+++ test/test_rpg.py
@@ -1,29 +1,30 @@
 from src.RPG import Character
 
 def test_player_initialization():
-    #ETANT DONNE UN PERSONNAGE, doit avec hp==100
-    p = Character("Guerrier", 20)
+    #ETANT DONNE UN PERSONNAGE, doit avec hp==10
+    p = Character("Guerrier")
 
-    assert p.hp == 100
+    assert p.hp == 10
 
 def test_attack_reduces_hp():
-    # ETANT DONNE deux personnages un monstre et un hero avec 100 PV
-    hero = Character("Hero", 20)
-    monster = Character("Gobelin", 5)
+    # ETANT DONNE deux personnages un monstre et un hero avec 10 PV
+    hero = Character("Hero")
+    monster = Character("Gobelin")
     
     #QUAND LE HERO ATTAQUE LE MONSTRE
     hero.attack(monster)
     
-    #LE MONSTRE doit avoir 100-20=80 hp
-    assert monster.hp == 80
+    #LE MONSTRE doit avoir 9 hp
+    assert monster.hp == 9
 
 def test_player_death():
     #ETANT DONNE UN PERSONNAGE
-    monster = Character("Gobelin", 5)
+    monster = Character("Gobelin")
 
-    #QUAND IL PREND DES DOMMAGES > A ses HP
-    monster.take_damage(110) 
+    #QUAND IL PREND DES 10 COUPS
+    for i in range(10) :
+        monster.take_damage()
     
-    #IL DOIT avoir 0 hp et ne pas être en vie 
+    #IL DOIT avoir 0 hp et ne plus être en vie
     assert monster.hp == 0
     assert monster.is_alive() is False
