e12b881109335fb1a2d98b3ea0b7ac82a9ed42c0
NOUDOUKOU <121809959+Florasika@users.noreply.github.com>
2026-03-03 14:21:39 +0000

    Add tests for Personnage class functionality

--- src/test_joeur.py
+++ src/test_joeur.py
@@ -1,7 +1,25 @@
-class Joeur:
-    def __init__(self):
-        self.hp = 100
-    def attack(self, other):
-        other.hp -= 10
-          if other.hp < 0:
-            other.hp = 0
+from personnage import Personnage
+
+
+def test_personnage_commence_avec_10_hp():
+    p = Personnage()
+    assert p.hp == 10
+
+
+def test_attaque_enleve_1_hp():
+    attaquant = Personnage()
+    defenseur = Personnage()
+
+    attaquant.attaquer(defenseur)
+
+    assert defenseur.hp == 9
+
+
+def test_personnage_meurt_a_zero():
+    p = Personnage()
+    p.hp = 1
+
+    attaquant = Personnage()
+    attaquant.attaquer(p)
+
+    assert p.est_vivant() is False
