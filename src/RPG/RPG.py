f0d0a9725048e13a1312604d2e4e371f4944a5c3
Theo Hadj <theo.hadja@gmail.com>
2026-03-04 16:25:11 +0100

    pushpush

---
+++ src/RPG/RPG.py
@@ -0,0 +1,114 @@
+import random
+
+class Character:
+    def __init__(self,level:int,end:int,force:int, agi:int, chn:int, armor:int = 0,  arme:int=1):
+        # self.name = name
+        # self.hp = hp
+        # self.attack_power = attack_power
+        if not (0 <= int(armor) <= 100):
+            raise ValueError("L'armure doit être comprise entre 0 et 100")
+        if (arme < 0):
+            raise ValueError("Le multiplicateur d'arme ne peut pas être négatif")
+        if(level<=0):
+            raise ValueError("Le niveau doit être strictement positif")
+
+        self.baseHp = 10
+
+        self.lvl = level
+        self.force = force
+        self.endurance = end
+        self.agi= 0
+        self.chn=0
+        #la chance marche comme suis => chaque tour quelqu'un esquive forcément parmis les chance une et tiré
+        self.hp = self.baseHp + self.endurance + 2*self.lvl
+        self.armor = armor        
+        self.arme = arme
+
+        self.maxHp = self.baseHp + self.endurance + 2*self.lvl
+
+
+                
+    def is_alive(self):
+        return self.hp > 0
+
+    def take_damage(self, amount):
+        if isinstance(amount, (int, float)) and amount > 0:
+            amount = amount * (1 - (self.armor/100))
+            if abs(amount % 1 - 0.5) < 1e-9:
+                amount = int(amount)
+            else:
+                amount=round(amount)
+                            
+            if(amount>0):
+                self.hp -= amount
+                if self.hp < 0:
+                    self.hp = 0
+
+    def attack(self, target):
+        if self.is_alive():
+            target.take_damage(random.randint(0, self.force + 1 + 2*self.lvl) * self.arme)
+    
+    def levelUp(self): #doesn't exists
+        self.lvl += 1
+        
+        
+class Equipe:
+    def __init__(self, perso1:Character, perso2:Character):
+        self.perso1 = perso1
+        self.perso2 = perso2
+
+    def isAlive(self):
+        if self.perso_1.hp==0 and self.perso_2.hp==0:
+            return False
+        else:
+            return True
+    
+    def whoLowest(self):
+        if(self.perso1.hp/self.perso1.maxHp > self.perso2.hp/self.perso2.maxHp):
+            return self.perso2
+        elif(self.perso1.hp/self.perso1.maxHp < self.perso2.hp/self.perso2.maxHp):
+            return self.perso1
+        else:
+            if(random.randint(1, 2) ==1):
+                return self.perso1
+            else:
+                return self.perso2
+
+            
+        
+            
+
+class Duel:
+    def __init__(self, equipe1:Equipe, equipe2:Equipe):
+        self.equipe1 = equipe1
+        self.equipe2 = equipe2
+        
+    def fight(self):
+
+        while(self.equipe1.isAlive==False or self.equipe2.isAlive==False):
+            order, firstEq, scEq = self.getOrder()
+            for p in order:
+                p.attack(scEq.whoLowest())
+       
+        if(self.equipe1.isAlive):
+            return 1
+        else:
+            return 2
+        
+    def getOrder(self):
+        tous_les_combattants = [
+        self.equipe1.perso_1, self.equipe1.perso_2,
+        self.equipe2.perso_1, self.equipe2.perso_2
+        ]
+                
+        p1 = max(tous_les_combattants, key=lambda p: p.agi)
+        eq_attaquante = self.equipe1 if p1 in [self.equipe1.perso_1, self.equipe1.perso_2] else self.equipe2
+        eq_adverse = self.equipe2 if eq_attaquante == self.equipe1 else self.equipe1
+        p2 = max([eq_adverse.perso_1, eq_adverse.perso_2], key=lambda p: p.agi)            
+        p3 = eq_attaquante.perso_2 if p1 == eq_attaquante.perso_1 else eq_attaquante.perso_1
+        p4 = eq_adverse.perso_2 if p2 == eq_adverse.perso_1 else eq_adverse.perso_1
+
+        return ([p1, p2, p3, p4], eq_attaquante, eq_adverse)
+    
+    def get_enemy_team(self, character):
+        return self.equipe2 if character in (self.equipe1.perso_1, self.equipe1.perso_2) else self.equipe1
--- test/test_rpg.py
+++ test/test_rpg.py
@@ -1,17 +1,21 @@
-from src.RPG import Character
+from src.RPG.RPG import Character, Equipe, Duel
+
 import pytest
+
+def create_default_char(armor=0, arme=1):
+    return Character(level=1, end=0, force=0, agi=0, chn=0, armor=armor, arme=arme)
 
 def test_player_initialization():
     #ETANT DONNE UN PERSONNAGE
-    p = Character()
+    p = create_default_char()
 
     #doit avoir hp==10 + endurance
-    assert p.hp == 10 + p.endurance
+    assert p.hp == 12 + p.endurance
 
 def test_attack_reduces_hp():
     # ETANT DONNE deux personnages un perso et un perso1 avec 10 PV
-    perso1 = Character()
-    perso2 = Character()
+    perso1 = create_default_char()
+    perso2 = create_default_char()
     hp0 = perso2.hp
 
     #QUAND LE perso1 ATTAQUE LE perso
@@ -23,50 +27,50 @@ def test_attack_reduces_hp():
 
 def test_player_death():
     #ETANT DONNE UN PERSONNAGE
-    perso2 = Character()
+    perso2 = create_default_char()
 
     #QUAND IL PREND ASSEZ DE COUPS
     while(perso2.hp>0) :
         perso2.take_damage(1)
-    
+
     #IL DOIT avoir 0 hp et ne plus être en vie
     assert perso2.hp == 0
     assert perso2.is_alive() is False
 
 def test_player_kill():
     #ETANT DONNE DEUX PERSONNAGES perso et perso1
-    perso1 = Character()
-    perso2 = Character()
+    perso1 = create_default_char()
+    perso2 = create_default_char()
 
     #QUAND perso TAPE perso1 JUSQU'A 0 HP
     while(perso1.hp>0) :
         perso2.attack(perso1)
-    
+
     #perso1 doit mourir et avoir 0 hp
     assert perso1.hp == 0
     assert perso1.is_alive() is False
 
 def test_player_over_damage():
     #ETANT DONNE DEUX PERSONNAGES perso et perso1
-    perso1 = Character()
-    perso2 = Character()
+    perso1 = create_default_char()
+    perso2 = create_default_char()
 
-    #QUAND perso TAPE ASSEZ LE perso1 POUR QU'IL MEURT, PUIS TAPE DEUX FOIS DE PLUS 
+    #QUAND perso TAPE ASSEZ LE perso1 POUR QU'IL MEURT, PUIS TAPE DEUX FOIS DE PLUS
     while(perso1.hp>0):
         perso2.attack(perso1)
-    
+
     for i in range (2):
         perso2.attack(perso1)
-        
+
     #perso1 doit mourir et ne pas perdre plus d'hp que 0
     assert perso1.hp == 0
     assert perso1.is_alive() is False
- 
+
 def test_dead_player_cant_attack():
-    #ETANT DONNE DEUX PERSONNAGES perso et perso1 
-    perso1 = Character()
+    #ETANT DONNE DEUX PERSONNAGES perso et perso1
+    perso1 = create_default_char()
     heroHp0=perso1.hp
-    perso2 = Character()
+    perso2 = create_default_char()
 
     #QUAND perso EST MORT ET QU'IL ATTAQUE
     while(perso2.hp>0):
@@ -77,143 +81,203 @@ def test_dead_player_cant_attack():
 
 def test_valid_take_damage_argument():
     # Etant donné un personnage
-    perso1 = Character()
+    perso1 = create_default_char()
     hero_base_hp = perso1.hp
-    
+
     # Lorsqu'un personnage subit des dégats, et que la valeur des dégats subits n'est pas un nombre supérieur à 0
     perso1.take_damage(-1)
-    
+
     # Alors le perso ne doit pas perdre points de vie
     assert perso1.hp == hero_base_hp
 
     perso1.take_damage("1")
     assert perso1.hp == hero_base_hp
-    
+
     perso1.take_damage(perso1)
     assert perso1.hp == hero_base_hp
 
 def test_has_endu():
     #Etant donné un personnage
-    perso1 = Character()
-    
-    baseEnd= perso1.endurance
+    perso1 = create_default_char()
+
     #Quand il vient d'être créé
     #Alors il a 0 d'endurance
     assert perso1.endurance == 0
 
-
-    #Quand il gagne un niveau
-    perso1.levelUp()
+    perso2 = Character(arme=1, armor=0, end=2, force=0, agi=0, chn=0, level=1)
     #Alors il gagne 2 d'endurance
-    assert perso1.endurance == 2
-    assert (baseEnd + 2) == 2
+    assert perso2.endurance == 2
+    
 
 def test_end_impact_hp():
     #Etant donné un personnage
-    perso1 = Character()
+    perso1 = create_default_char()
 
     #Alors ces hp sont égaux à baseHp + son endurance
-    assert perso1.hp == perso1.baseHp + perso1.endurance 
-    
-def test_levelUp():
-    #Etant donné un personnage
-    perso1 = Character()
-    
-    damage0= perso1.force
-    hp0= perso1.hp
+    assert perso1.hp == 10 + perso1.lvl*2 + perso1.endurance
 
-    #Quand il gagne un niveau
-    perso1.levelUp()
+# def test_levelUp():
+#     #Etant donné un personnage
+#     perso1 = create_default_char()
 
-    #Alors ses dégats et ses hp augmente de deux
-    assert perso1.force == damage0+2
-    assert perso1.hp == hp0+2
+#     damage0= perso1.force
+#     hp0= perso1.hp
 
-    #Quand il gagne un autre niveau
-    perso1.levelUp()
+#     #Quand il gagne un niveau
+#     perso1.levelUp()
 
-    #Alors ses dégats et ses hp augmente de quatre (2*lvl)
-    assert perso1.force == damage0+4
-    assert perso1.hp == hp0+4
-     
+#     #Alors ses dégats et ses hp augmente de deux
+#     assert perso1.force == damage0+2
+#     assert perso1.hp == hp0+2
+
+#     #Quand il gagne un autre niveau
+#     perso1.levelUp()
+
+#     #Alors ses dégats et ses hp augmente de quatre (2*lvl)
+#     assert perso1.force == damage0+4
+#     assert perso1.hp == hp0+4
+
 
 
 def test_caracteristique_force():
     # Étant donné un personnage
-    perso = Character()
-    perso_2 = Character()
+    perso = create_default_char()
+    perso2 = create_default_char()
 
     perso_force = perso.force
     perso_base_hp = perso.hp
-    
+
     # Il possède une caractéristique Force qui augmente ses dégats
     assert perso_force >= 0
-    perso_2.attack(perso)
+    perso2.attack(perso)
     assert perso.hp <= perso_base_hp
 
 def test_armor_is_over_powered():
-    #Etant donné un perso1 possédant une armure de 102 
-    perso1 = Character(102)
-    
+    #Etant donné un perso1 possédant une armure de 102
+    with pytest.raises(ValueError):
+        create_default_char(101,1)
     #Le héro ne peut pas être créer.
-    assert perso1 == False 
 
-def test_armor_is_over_powered():
-    #Etant donné un perso1 possédant une armure de 102 
-    with pytest.raises(ValueError):
-        Character(102)
-    
-    #Le héro ne peut pas être créer.
+
 def test_armor_is_negative():
-    #Etant donné un perso1 possédant une armure de -2 
+    #Etant donné un perso1 possédant une armure de -2
     with pytest.raises(ValueError):
-        Character(102)
-    
+        create_default_char(-2,1)
+
     #Le héro ne peut pas être créer.
-     
+
 
 def test_is_armor_reducing_damage_taken():
-    #Etant donné un perso1 possédant une armure de 50 
-    perso1 = Character(50)
+    #Etant donné un perso1 possédant une armure de 50
+    perso1 = create_default_char(50,1)
+
     hp0= perso1.hp
 
     #Quand heros reçoit des dommages égale à 2 de dégats
     perso1.take_damage(2)
-    
+
     #Le héro doit perdre 1 hp.
-    assert (hp0 - 1)== perso1.hp 
-    
+    assert (hp0 - 1)== perso1.hp
+
 def test_is_max_armor_protect_damage_taken():
-    #Etant donné un perso1 possédant une armure de 1 
-    perso1 = Character(100)
+    #Etant donné un perso1 possédant une armure de 1
+    perso1 = create_default_char(100,1)
     hp0= perso1.hp
 
     #Quand heros reçoit des dommages égale à 1 de dégats
     perso1.take_damage(1)
-    
+
     #la vie de perso1 ne doit pas changer.
-    assert hp0 == perso1.hp 
-    
+    assert hp0 == perso1.hp
+
 def test_is_armor_reducing_attack_receive():
-    #Etant donné deux personnages, perso1 possédant une armure de 1 et perso 
-    perso1 = Character(50)
+    #Etant donné deux personnages, perso1 possédant une armure de 50 et perso
+    perso1 = create_default_char(50,1)
     h0=perso1.hp
-    perso = Character()
+    perso = create_default_char()
 
     #Quand heros reçoit une attaque de perso qui a 0 de force et qui fera entre 0 et 1 de dégats
     perso.attack(perso1)
-    
+
     #La vie du perso1 ne doit pas changer.
 
-    assert perso1.hp == h0 
-    
+    assert perso1.hp == h0
+
 def test_armor_is_reducing_more_than_received():
-    #Etant donné un perso1 possédant une armure de 3 
-    perso1 = Character(100)
+    #Etant donné un perso1 possédant une armure de 100
+    perso1 = create_default_char(100,1)
+
     hp0= perso1.hp
 
-    #Quand heros reçoit des dommages égale à 1 de dégats
+    #Quand heros reçoit des dommages égale à 0 de dégats
     perso1.take_damage(0)
-    
+
     #La vie du perso1 ne doit pas changer.
-    assert hp0 == perso1.hp 
+    assert hp0 == perso1.hp
+
+# def test_demo_arme_attaque_hausse():
+#     # Étant donné un personnage ayant une arme et un perso2
+#     perso_arme = create_default_char(0, 1.2)
+#     perso_sans_arme = create_default_char(0,1.2)
+#     perso2 = create_default_char(0,1.2)
+#     ennemi2 = create_default_char(0,1.2)
+
+#     # Lorsque le personnage armé attaque l'perso2
+#     perso_arme.attack(perso2)
+#     perso_sans_arme.attack(ennemi2)
+
+#     # Alors l'perso2 subit plus de dégâts que lors d'une attaque sans arme
+#     assert perso2.hp <= 10 and perso2.hp >= 4
+#     assert ennemi2.hp <= 10 and ennemi2.hp >= 5
+
+
+# def test_demo_arme_attaque_baisse():
+#     # Étant donné un personnage ayant une arme et un autre personnage
+#     perso_arme = create_default_char(0, 0.5)
+#     perso_sans_arme = create_default_char(0,1.2)
+#     perso2 = create_default_char(0,1.2)
+#     ennemi2 = create_default_char(0,1.2)
+
+#     # Lorsque le personnage armé attaque le second personnage
+#     perso_arme.attack(perso2)
+#     perso_sans_arme.attack(ennemi2)
+
+#     # Alors l'perso2 subit moins de dégâts que lors d'une attaque sans arme
+#     assert perso2.hp <= 10 and perso2.hp >= 2.5
+#     assert ennemi2.hp <= 10 and ennemi2.hp >= 5
+
+
+def test_deux_persos_armures():
+    #Etant donné trois personnages, perso1 possédant une armure de 50, un perso1 sans et perso
+    perso1 = create_default_char(50,1)
+    h0=perso1.hp
+
+    perso2 = create_default_char(0,1)
+    h02=perso2.hp
+
+    perso = create_default_char(0,1.2)
+
+    #Quand heros reçoit une attaque de perso qui a 0 de force et qui fera entre 0 et 1 de dégats
+    perso.attack(perso1)
+    perso.attack(perso2)
+
+    #La vie du perso1 ne doit pas changer.
+
+    assert perso1.hp == h0
+    assert perso1.hp != perso2.hp
+
+def test_demo_duel_gagne():
+    # Étant un duel
+    eq1_p1 = create_default_char(50, 1.5)
+    eq1_p2 = create_default_char(0, 1)
+    eq2_p1 = create_default_char(0, 1.1)
+    eq2_p2 = create_default_char(20, 1.2)
+    equipe1 = Equipe(eq1_p1, eq1_p2)
+    equipe2 = Equipe(eq2_p1, eq2_p2)
+    duel = Duel(equipe1, equipe2)
+
+    #Quand le combat a lieu
+    winner = duel.fight()
+
+    #Alors il doit y avoir un gagnant
+    assert winner in [1, 2]
