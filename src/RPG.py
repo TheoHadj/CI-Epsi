ea6123408f2c6428c1ef6e67700d61eabae8c8ed
tajMahel <maelmemain1@gmail.com>
2026-03-04 11:56:18 +0100

    ajout arme

--- src/RPG.py
+++ src/RPG.py
@@ -1,8 +1,10 @@
 import random
+from typing import Literal
 
 
 class Character:
-    def __init__(self):
+    
+    def __init__(self, arme_multiplicator:int=1):
         # self.name = name
         # self.hp = hp
         # self.attack_power = attack_power
@@ -15,9 +17,12 @@ class Character:
         self.force = 0
         self.endurance = 0
         self.hp = 10
-
+        self.arme_multiplicator = arme_multiplicator if arme_multiplicator > 0 else 1
         self.attack
-        
+    
+    def ajouter_arme(self, arme_multiplicator:int):
+        self.arme_multiplicator = arme_multiplicator if arme_multiplicator > 0 else 1
+                
     def is_alive(self):
         return self.hp > 0
 
@@ -30,8 +35,7 @@ class Character:
 
     def attack(self, target):
         if self.is_alive():
-            target.take_damage(random.randint(0, self.force + 1))
-    
+            target.take_damage(random.randint(0, self.force + 1) * self.arme_multiplicator)
     
     def levelUp(self):
         self.lvl += 1
